dfd3423440f2c66cce26825d728b281545d76c1f
pjukka <pjukka@users.noreply.github.com>
2018-08-13 12:20:19 +0300

    Add a auto test case for Query class to test 'bbox' query parameter.

--- test/unit_test/query.cpp
+++ test/unit_test/query.cpp
@@ -123,6 +123,51 @@ BOOST_AUTO_TEST_CASE(
   BOOST_CHECK_THROW({ Query query3(request, authEngine, config); }, Spine::Exception);
   request.removeParameter("places");
 }
+
+BOOST_AUTO_TEST_CASE(
+    query_constructor_parseLocationOptions_bbox,
+    *boost::unit_test::depends_on("query_constructor_allowMultipleLocationOptions_enabled"))
+{
+  BOOST_CHECK(authEngine != nullptr);
+
+  const std::string filename = "cnf/aviplugin.conf";
+  std::unique_ptr<Config> config(new Config(filename));
+  Spine::HTTP::Request request;
+  request.addParameter("param", "value");
+
+  // Exception: Option maxdistance is required with latlon/lonlat, bbox and wkt options
+  request.addParameter("bbox", "25,60,26,61");
+  BOOST_CHECK_THROW({ Query query1(request, authEngine, config); }, Spine::Exception);
+
+  // One bbox by using integers
+  request.addParameter("maxdistance", "1000.0");
+  Query query2(request, authEngine, config);
+  BOOST_CHECK_EQUAL(query2.itsQueryOptions.itsLocationOptions.itsBBoxes.size(), 1);
+  request.removeParameter("bbox");
+
+  // One bbox by using floating point mumbers
+  request.addParameter("bbox", "25.11 ,60.1, 26.0,61.1");
+  Query query3(request, authEngine, config);
+  BOOST_CHECK_EQUAL(query3.itsQueryOptions.itsLocationOptions.itsBBoxes.size(), 1);
+  request.removeParameter("bbox");
+
+  // Exception: 4 values required for option 'bbox'; '25,60,26'
+  request.addParameter("bbox", "25,60,26");
+  BOOST_CHECK_THROW({ Query query4(request, authEngine, config); }, Spine::Exception);
+  request.removeParameter("bbox");
+
+  // Exception: 4 values required for option 'bbox'; '25,60,26,61,27'
+  request.addParameter("bbox", "25,60,26,61,27");
+  BOOST_CHECK_THROW({ Query query5(request, authEngine, config); }, Spine::Exception);
+  request.removeParameter("bbox");
+
+  // Two bboxes
+  request.addParameter("bbox", "25,60,26,61");
+  request.addParameter("bbox", "19,60,20,61");
+  Query query6(request, authEngine, config);
+  BOOST_CHECK_EQUAL(query6.itsQueryOptions.itsLocationOptions.itsBBoxes.size(), 2);
+  request.removeParameter("bbox");
+}
 }  // namespace Avi
 }  // namespace Plugin
 }  // namespace SmartMet
