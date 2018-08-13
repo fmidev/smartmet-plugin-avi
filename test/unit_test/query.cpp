c124d42955db1eb6e1f9cbd5207646e2247a91d0
pjukka <pjukka@users.noreply.github.com>
2018-08-13 09:37:01 +0300

    Add unit tests for Query class to test the multiplelocationoptions setting of the Plugin.

--- test/unit_test/query.cpp
+++ test/unit_test/query.cpp
@@ -60,6 +60,39 @@ BOOST_AUTO_TEST_CASE(query_constructor, *boost::unit_test::depends_on("query_aut
   request.removeParameter("param");
   BOOST_CHECK_THROW({ Query query2(request, authEngine, config); }, Spine::Exception);
 }
+
+BOOST_AUTO_TEST_CASE(query_constructor_allowMultipleLocationOptions_enabled,
+                     *boost::unit_test::depends_on("query_constructor"))
+{
+  BOOST_CHECK(authEngine != nullptr);
+
+  const std::string filename = "cnf/aviplugin.conf";
+  std::unique_ptr<Config> config(new Config(filename));
+  Spine::HTTP::Request request;
+  request.addParameter("param", "value");
+  request.addParameter("place", "Göteborg");
+  Query query1(request, authEngine, config);
+
+  request.addParameter("icao", "EFHK");
+  Query query2(request, authEngine, config);
+}
+
+BOOST_AUTO_TEST_CASE(query_constructor_allowMultipleLocationOptions_disabled,
+                     *boost::unit_test::depends_on("query_constructor"))
+{
+  BOOST_CHECK(authEngine != nullptr);
+
+  const std::string filename = "cnf/aviplugin-with-authentication.conf";
+  std::unique_ptr<Config> config(new Config(filename));
+  Spine::HTTP::Request request;
+  request.addParameter("param", "value");
+  request.addParameter("place", "Göteborg");
+  Query query1(request, authEngine, config);
+
+  // The query does not work with multiple location options.
+  request.addParameter("icao", "EFHK");
+  BOOST_CHECK_THROW({ Query query2(request, authEngine, config); }, Spine::Exception);
+}
 }  // namespace Avi
 }  // namespace Plugin
 }  // namespace SmartMet
